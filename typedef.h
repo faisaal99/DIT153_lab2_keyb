bb6ed78a82bf710acf551d4160e8fed5b520ce0b
Faisal A <faisal.163@hotmail.com>
2023-02-07 17:54:06 +0100

    Re-wrote #define's to typedef's.

--- typedef.h
+++ typedef.h
@@ -1,11 +1,13 @@
 #ifndef _TYPEDEF_H_
 #define _TYPEDEF_H_
 
-#define uchar unsigned char
-#define ushort unsigned short
-#define ulong unsigned long
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned int   uint;
+typedef unsigned long  ulong;
+typedef int            bool;
+
 #define true 1
 #define false 0
-#define bool int
 
 #endif // _TYPEDEF_H_
