c46a650ef00d6ce40743b7433a5b15bda27b1a6e
Faisal A <faisal.163@hotmail.com>
2023-02-07 17:55:13 +0100

    Add GPIOD and GPIOE reigsters

--- gpio.h
+++ gpio.h
@@ -1,9 +1,31 @@
 #ifndef _KEYB_H_
 #define _KEYB_H_
 
-#define GPIOD 0x40020C00
-#define GPIOD_MODER (GPIOD)
-#define GPIOD_OTYPER (GPIOD + 4)
-// TODO: Not Finished
+
+/* GPIOD */
+
+#define GPIOD            0x40020C00
+#define GPIOD_MODER_LOW  (GPIOD)
+#define GPIOD_MODER_HIGH (GPIOD +  2)
+#define GPIOD_OTYPER     (GPIOD +  4)
+#define GPIOD_OSPEEDR    (GPIOD +  8)
+#define GPIOD_PUPDR      (GPIOD + 12)
+#define GPIOD_IDR        (GPIOD + 16)
+#define GPIOD_ODR        (GPIOD + 20)
+#define GPIOD_BSRR       (GPIOD + 24)
+
+
+/* GPIOE */
+
+#define GPIOE            0x40021000
+#define GPIOE_MODER_LOW  (GPIOE)
+#define GPIOE_MODER_HIGH (GPIOE +  2)
+#define GPIOE_OTYPER     (GPIOE +  4)
+#define GPIOE_OSPEEDR    (GPIOE +  8)
+#define GPIOE_PUPDR      (GPIOE + 12)
+#define GPIOE_IDR        (GPIOE + 16)
+#define GPIOE_ODR        (GPIOE + 20)
+#define GPIOE_BSRR       (GPIOE + 24)
+
 
 #endif // _KEYB_H_
