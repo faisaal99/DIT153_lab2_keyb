c067d10f7ec5246bda9c67bdae2e0b7576f91f62
Faisal A <faisal.163@hotmail.com>
2023-02-07 17:56:43 +0100

    Change ints to unsigned ints. Start work on read_column()

--- startup.c
+++ startup.c
@@ -26,22 +26,23 @@ __asm__ volatile(".L1: B .L1\n");				/* never return */
 /**
  * @brief Run code to initialize program and do important preparations.
  */
-void app_init(void) // TODO:
+void app_init(void) // TODO
 {
     // Start clocks for port D and port E.
-    *(ulong*)0x40023830 = 0x18;
-    // Set the address on the left to the value on the right;
-    *(ulong*)GPIOD = 0x00005555;
+    *(volatile ulong*)0x40023830 = 0x18;
+    // Set port D to medium speed.
+    *(volatile uint*)0x40020C08 = 0x55555555;
+
 }
 
 
 // =============================================================================
 // FUNCTIONS
 
-void activate_row(int);     // Activates a specific row.
-int read_column();          // Reads each column and returns the index of the
-                            // key being pressed, if that.
-uchar key_value(int, int);  // Return the value of the key being pressed.
+void activate_row(uint);      // Activates a specific row.
+int read_column();            // Reads each column and returns the index of the
+                              // key being pressed, if that.
+uchar key_value(uint, uint);  // Return the value of the key being pressed.
 
 
 /**
@@ -50,10 +51,10 @@ uchar key_value(int, int);  // Return the value of the key being pressed.
  */
 uchar keyb(void)
 {
-    for (int row = 1; row <= 4; row++)
+    for (uint row = 1; row <= 4; row++)
     {
         activate_row(row);
-        int column = read_column();
+        uint column = read_column();
         if (column)
             return key_value(row, column);
     }
@@ -66,7 +67,7 @@ uchar keyb(void)
  * @brief TODO
  * @param row The current row to index. Is 1-indexed.
  */
-void activate_row(int row)
+void activate_row(uint row)
 {
 
 }
@@ -77,7 +78,11 @@ void activate_row(int row)
  */
 int read_column()
 {
+    for (uint col = 1; col <= 4; col++)
+        if ( 0/* Some way of determining if shit is active */ )
+            return col;
 
+    return 0;
 }
 
 
@@ -86,7 +91,7 @@ int read_column()
  */
 void out_7_seg(uchar keycode)
 {
-
+    // TODO
 }
 
 
@@ -95,7 +100,7 @@ void out_7_seg(uchar keycode)
  * @param row The row of the key being pressed.
  * @param col The column of the key being pressed.
  */
-uchar key_value(int row, int col)
+uchar key_value(uint row, uint col)
 {
     // TODO
 }
